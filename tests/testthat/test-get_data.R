eb9717ceb459230735270f974c146d905e2e5b33
Vadim Taskaev <vadim.taskaev@outlook.com>
2022-01-27 21:33:19 -0800

    feat: fixed package referencing in testthat.R to avoid issues with dependencies

--- tests/testthat/test-get_data.R
+++ tests/testthat/test-get_data.R
@@ -4,21 +4,21 @@ sample_df <- readr::read_csv(
 
 actual_df <- get_data()
 
-test_that("column names of downloaded data do match those of sample data", {
+testthat::test_that("column names of downloaded data do match those of sample data", {
   expect_equal(
     sample_df |> colnames(),
     actual_df |> colnames()
     )
 })
 
-test_that("data types names of downloaded data do match those of sample data", {
+testthat::test_that("data types names of downloaded data do match those of sample data", {
   expect_equal(
     sample_df |> sapply(class),
     actual_df |> sapply(class)
   )
 })
 
-test_that("most recent data from 7 or more days ago", {
+testthat::test_that("most recent data from 7 or more days ago", {
   expect_true(
     Sys.Date() - actual_df$Reported_Date |> max() < 7
   )
